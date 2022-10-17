d7855c3b33e2cc75fb144536237c562c40bc1d8e
Enamulhqenam <enamulhqenam50607210@gmail.com>
2022-10-17 07:27:08 +0600

    change file and photo upload directory

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=1, initial-scale=1.0">
+	<title>Uploader</title>
+</head>
+<body>
+	<form action="upload.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<button type="submit" name="submit">UPLOAD</button>
+	</form>
+</body>
+</html>
